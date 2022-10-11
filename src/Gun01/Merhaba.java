c21bf3eeb2768aa5bfb8a325d1aa7f9a0bd7f723
rovshan.ashirov <rovshanashirov@yahoo.com>
2022-10-11 10:19:09 +0400

    ikinci taski yaptim

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -2,7 +2,11 @@ package Gun01;
 
 public class Merhaba {
     public static void main(String[] args) {
+        System.out.println(" Merhaba 1 ");
 
+        for (int i=0; i<5;i++){
+            System.out.println("Merhaba " + i);
+        }
     }
 
 }
