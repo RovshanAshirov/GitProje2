d2ba908fc8b96f1da5006c0944837acb32e0870f
rovshan.ashirov <rovshanashirov@yahoo.com>
2022-10-11 10:46:44 +0400

    dorduncu taskimi yaptim

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -3,16 +3,11 @@ package Gun01;
 public class Merhaba {
     public static void main(String[] args) {
 
-
-        for (int i=0; i<5;i++){
+        System.out.println("Merhaba ");
+        for (int i = 0; i < 5; i++) {
             System.out.println("Merhaba " + i);
         }
+    }}
 
-        int sayac=0;
-        do {
-            System.out.println(" yeni merhaba ");
-            sayac++;
-        }while (sayac<5);
-    }
 
-}
+
